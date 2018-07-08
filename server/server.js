8d999c0ae23e9c10b348729a4b7ad9a28aab9f92
Tachakorn Uengpaporn <tachakorn.uengpaporn@gmail.com>
2018-07-09 00:25:49 +0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,18 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find({}).then((todos) => {
+        res.send({
+            status_code: 200,
+            message: 'OK',
+            todos
+        })
+    }, (err) => {
+        res.status(400).send(err)
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server started on port 3000')
 })
